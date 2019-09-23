1eeb6278a8db33f99e78dae4af96a14487d6324f
Lucenajj <lucenajj@gmail.com>
2019-09-22 22:06:42 -0300

    querySelector

---
+++ script/controller/CalcController.js
@@ -0,0 +1,57 @@
+class CalcController{
+     constructor(){
+         this.locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");         
+         this.currentDate;
+         this.initialize();
+     }
+
+     initialize(){
+         this.setDisplayDateTime();
+        setInterval(()=>{
+            this.setDisplayDateTime();
+
+        },1000);
+       
+     }
+
+     initButtonsEvents(){
+         let buttons = document.querySelectorAll("#buttons > g, #parts > g ")
+     }
+
+     setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+     }
+     get displayDate(){
+        return this._dateEl.innerHTML;
+     }
+     set displayDate(value){
+        return this._dateEl.innerHTML = value;
+     }
+     get displayTime(){
+        return this._timeEl.innerHTML;
+     }
+     set displayTime(value){
+        return this._timeEl.innerHTML = value;
+     }
+
+     get displayCalc(){
+         return this._displayCalcEl.innerHTML;
+     }
+     set displayCalc(value){
+         this._displayCalcEl.innerHTML = value;
+     }
+     get currentDate(){
+        return new Date();
+    }
+    set currentDate(value){
+        this.currentDate = value;
+    }
+} 
